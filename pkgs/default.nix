0f71e59af7817deeb60511a95c0d0b7e8c7d6bd9
Landon Wu <tywtyw2002@gmail.com>
2023-04-25 01:09:46 -0600

    feat(pkgs:starship): add aarch64 linux

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,3 +1,3 @@
 { pkgs ? import <nixpkgs> { } }: {
-    starship-c = pkgs.callPackage ./starship-c {};
+  starship-c = pkgs.callPackage ./starship-c { };
 }
--- pkgs/starship-c/default.nix
+++ pkgs/starship-c/default.nix
@@ -2,20 +2,26 @@
 , lib
 , installShellFiles
 , fetchurl
+,
 }:
 stdenv.mkDerivation rec {
   name = "starship-c-${version}";
-  version = "c20230304";
+  version = "c20230425";
 
   src = fetchurl (
     if stdenv.system == "x86_64-darwin"
     then {
       url = "https://github.com/tywtyw2002/starship/releases/download/${version}/starship-x86_64-apple-darwin.tar.gz";
-      sha256 = "59a1236d7e44351a714cc26171646401c9f30b00b505e1afc279866a87dbda48";
+      sha256 = "ddc259c9479c2f175791e902591f1a7fdf2a54b0278fa0cd396552db9e034214";
+    }
+    else if stdenv.system == "aarch64-linux"
+    then {
+      url = "https://github.com/tywtyw2002/starship/releases/download/${version}/starship-aarch64-unknown-linux-gnu.tar.gz";
+      sha256 = "0c70b9778f085358dccde05add1edaa14c2175fc216c94e160702b0588d6e180";
     }
     else {
       url = "https://github.com/tywtyw2002/starship/releases/download/${version}/starship-x86_64-unknown-linux-gnu.tar.gz";
-      sha256 = "0gfjkd5nk95m19rdv4nfamlgq7xw8fvhndn4ddw34slwsnprwr6l";
+      sha256 = "c82bdeb94b50e847ca1659af611eb947c0a038652db06f4551a7523fe66dcb66";
     }
   );
 
@@ -31,11 +37,13 @@ stdenv.mkDerivation rec {
   '';
 
   preFixup =
-    if stdenv.isLinux then ''
+    if stdenv.isLinux
+    then ''
       patchelf \
         --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
         $out/bin/starship
-    '' else "";
+    ''
+    else "";
 
   postInstall = ''
     installShellCompletion --cmd starship \
@@ -48,6 +56,6 @@ stdenv.mkDerivation rec {
     description = "A minimal, blazing fast, and extremely customizable prompt for any shell";
     homepage = "https://starship.rs";
     # maintainers = with maintainers; [ tywtyw2002 ];
-    platforms = [ "x86_64-linux" "x86_64-darwin" ];
+    platforms = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" ];
   };
 }
