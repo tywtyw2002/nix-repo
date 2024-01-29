e13a7bd18d09fd0c3f953c1d75c80388033e2e03
tywtyw2002 <tywtyw2002@gmail.com>
2024-01-29 16:34:59 -0700

    chore(pkgs): gost (#38)

--- pkgs/gost/default.nix
+++ pkgs/gost/default.nix
@@ -5,16 +5,16 @@
 }:
 buildGoModule rec {
   pname = "gost";
-  version = "3.0.0-rc8";
+  version = "3.0.0-rc10";
 
   src = fetchFromGitHub {
     owner = "go-gost";
     repo = "gost";
     rev = "v${version}";
-    sha256 = "sha256-plFmQ/npIasPFpy8rKuHZFUKPsokiRep8j4N6Ls8PLo=";
+    sha256 = "sha256-o0lE6grS7beslw/O0AtmPxeWlA4z8XAaLlo41U6zAsg=";
   };
 
-  vendorSha256 = "sha256-KO+JmDoJDXgS4luXCzcawhMUr1+o0kRAjQA8+YiYVfM=";
+  vendorSha256 = "sha256-C4HPOPveU+pOd8gyZWSo9R5EAHh+RvrfqtlpiJoeW84=";
 
   meta = with lib; {
     description = "A simple tunnel written in golang";
